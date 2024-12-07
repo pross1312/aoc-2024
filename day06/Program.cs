d20cddb62e4d20f41b79b31d2b0d1fd831081015
dvtuong <dangvinhtuong12@gmail.com>
2024-12-07 11:39:47 +0700

    day06: part 2 continue, a seemingly better solution but wrong again

--- day06/Program.cs
+++ day06/Program.cs
@@ -18,35 +18,58 @@ void part2(string filePath) {
     Func<int, int, bool> isValidPosition = (int col, int row) => row >= 0 && row < lines.Length && col >= 0 && col < lines[row].Length;
     ((int x, int y), (int vx, int vy)) = findStart();
 
-    Queue<(int, int)> blocks = new();
+    List<((int, int), (int, int))> turningPoints = new();
+    Func<int, int, (int, int)> turnRight = (_vx, _vy) => (_vx, _vy) switch {
+        (0, -1) => (1, 0),
+        (1, 0) => (0, 1),
+        (0, 1) => (-1, 0),
+        (-1, 0) => (0, -1),
+        _ => (0, 0)
+    };
+
+    Func<int, int, bool> canExit = (bx, by) => {
+        lines[by][bx] = '#';
+        (int tempX, int tempY) = (x, y);
+        (int tempVX, int tempVY) = (vx, vy);
+        int count = turningPoints.Count;
+        while (isValidPosition(tempX, tempY)) {
+            while (isValidPosition(tempX+tempVX, tempY+tempVY) && lines[tempY+tempVY][tempX+tempVX] == '#') {
+                if (turningPoints.Contains(((tempX, tempY), (tempVX, tempVY)))) {
+                    lines[by][bx] = '.';
+                    turningPoints.RemoveRange(count, turningPoints.Count-count);
+                    Debug.Assert(turningPoints.Count == count);
+                    return false;
+                } else {
+                    turningPoints.Add(((tempX, tempY), (tempVX, tempVY)));
+                    (tempVX, tempVY) = turnRight(tempVX, tempVY);
+                }
+            }
+            tempX += tempVX;
+            tempY += tempVY;
+        }
+        turningPoints.RemoveRange(count, turningPoints.Count-count);
+        Debug.Assert(turningPoints.Count == count);
+        lines[by][bx] = '.';
+        return true;
+    };
+
     int blockCount = 0;
     do {
-        if (isValidPosition(x+vx, y+vy) && lines[y+vy][x+vx] == '#') {
-            blocks.Enqueue((x+vx, y+vy));
-            (vx, vy) = (vx, vy) switch {
-                (0, -1) => (1, 0),
-                (1, 0) => (0, 1),
-                (0, 1) => (-1, 0),
-                (-1, 0) => (0, -1),
-                _ => (0, 0)
-            };
-
-
-
-
-            if (blocks.Count == 3) {
-                (int bx, int by) = blocks.Dequeue();
-                (int newBx, int newBy) = (vx, vy) switch {
-                    (0, -1) => (x, by-1),
-                    (1, 0) => (bx+1, y),
-                    (0, 1) => (x, by+1),
-                    (-1, 0) => (bx-1, y),
-                    _ => (-1, -1),
-                };
-                Console.WriteLine($"Old {(bx, by)}, New {(newBx, newBy)}");
-                if (isValidPosition(newBx, newBy) && lines[newBy][newBx] != 'O') {
-                    (int tempX, int tempY) = (x+vx, y+vy);
+        while (isValidPosition(x+vx, y+vy) && lines[y+vy][x+vx] == '#') {
+            turningPoints.Add(((x, y), (vx, vy)));
+            (vx, vy) = turnRight(vx, vy);
+            if (turningPoints.Count >= 3) {
+                for (int i = 0; i < turningPoints.Count - 2; i++) {
+                    ((int px, int py), var _) = turningPoints[i];
+                    (int newBx, int newBy) = (vx, vy) switch {
+                        (0, -1) => (x, py-1),
+                        (1, 0) => (px+1, y),
+                        (0, 1) => (x, py+1),
+                        (-1, 0) => (px-1, y),
+                        _ => (-1, -1),
+                    };
                     bool isBlocked = false;
+                    (int tempX, int tempY) = (x+vx, y+vy);
                     while (isValidPosition(tempX, tempY) && (tempX != newBx || tempY != newBy)) {
                         if (lines[tempY][tempX] == '#') {
                             isBlocked = true;
@@ -55,9 +78,9 @@ void part2(string filePath) {
                         tempX += vx;
                         tempY += vy;
                     }
-                    if (!isBlocked) {
-                        blockCount++;
+                    if (isValidPosition(newBx, newBy) && !isBlocked && lines[newBy][newBx] == '.' && !canExit(newBx, newBy)) {
                         lines[newBy][newBx] = 'O';
+                        blockCount++;
                     }
                 }
             }
@@ -65,8 +88,7 @@ void part2(string filePath) {
         x += vx;
         y += vy;
     } while(isValidPosition(x, y));
-    Console.WriteLine($"{String.Join('\n', lines.Select(line => new String(line)).ToArray())}");
     Console.WriteLine($"Part 2: {blockCount}");
 }
 
-part2("sample.txt");
+part2("input.txt");
